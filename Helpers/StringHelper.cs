bdbb0c7200a21120a130ec010cbe4db8bcf08544
Luiz Henrique Strobelt Pérez <lhsperez@gmail.com>
2013-09-19 00:46:49 -0300

    StringHelper CountWords

--- Helpers/StringHelper.cs
+++ Helpers/StringHelper.cs
@@ -12,7 +12,7 @@ namespace Helpers
         /// Reports the zero-based index of the first ocurrence of the specified Unicode character in this string plus one.
         /// </summary>
         /// <param name="value">A Unicode character to seek.</param>
-        /// <returns></returns>
+        /// <returns>The zero-based index of the first ocurrence of the specified Unicode character in this string plus one.</returns>
         public static int RightIndexOf(this string source, char value)
         {
             return source.IndexOf(value) + 1;
@@ -22,7 +22,7 @@ namespace Helpers
         /// Reports the zero-based index of the first ocurrence of the specified string in this instance plus the length of it.
         /// </summary>
         /// <param name="value">The string to seek.</param>
-        /// <returns></returns>
+        /// <returns>The zero-based index of the first ocurrence of the specified string in this instance plus the length of it.</returns>
         public static int RightIndexOf(this string source, string value)
         {
             return source.IndexOf(value) + value.Length;
@@ -32,7 +32,7 @@ namespace Helpers
         /// Returns the number of ocurrences of the specified Unicode character in this string.
         /// </summary>
         /// <param name="value">A Unicode character to count ocurrences.</param>
-        /// <returns></returns>
+        /// <returns>The number of ocurrences of the specified Unicode character in this string.</returns>
         public static int CountOcurrences(this string source, char value)
         {
             int ocurrences = 0;
@@ -51,7 +51,7 @@ namespace Helpers
         /// Returns the number of ocurrences of the specified string in this string.
         /// </summary>
         /// <param name="value">The string to count ocurrences.</param>
-        /// <returns></returns>
+        /// <returns>The number of ocurrences of the specified string in this string.</returns>
         public static int CountOcurrences(this string source, string value)
         {
             int ocurrences = 0;
@@ -64,6 +64,17 @@ namespace Helpers
             }
 
             return ocurrences;
+        }
+
+        private static char[] separators = new char[] { ' ', ',', '.', ';', ':', '?', '!', '-', '_', '(', ')', '[', ']', '{', '}', '\n', '\r', '\t' };
+        /// <summary>
+        /// Returns the number of words from the string.
+        /// </summary>
+        /// <param name="source">The source string.</param>
+        /// <returns>The number of words on the string.</returns>
+        public static int CountWords(this string source)
+        {
+            return source.Split(separators).Length;
         }
     }
 }
